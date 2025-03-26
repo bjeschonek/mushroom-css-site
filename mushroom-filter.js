42f4be8e3dbbc4265b466180c2b4ddc23e14c1f1
Brad Jeschonek <blj747@gmail.com>
2025-03-25 21:22:28 -0400

    Added filtering to mushroom cards

---
+++ mushroom-filter.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function filterMushrooms() {
+    const mushroomCards = document.querySelectorAll('.mushroom-guide .card');
+    const seasonalSelect = document.querySelector('#season');
+    const edibleSelect = document.querySelector('#edible');
+    const noResultsMsg = this.documentElement.querySelector('.no-matches');
+
+    const currentFilters = {
+        season: 'all',
+        edible: 'all',
+    };
+
+    seasonalSelect.addEventListener('change', updateFilter);
+    edibleSelect.addEventListener('change', updateFilter);
+
+    function updateFilter(event) {
+        const filterType = event.target.name;
+        currentFilters[filterType] = event.target.value;+
+
+        filterCards();
+    }
+
+    function filterCards() {
+        let hasVisibleCards = false;
+
+        mushroomCards.forEach((card) => {
+            const season = card.querySelector('[data-season]').dataset.season;
+            const edible = card.querySelector('[data-edible]').dataset.edible;
+
+            const matchesSeason = currentFilters.season == season;
+            const matchesEdible = currentFilters.edible == edible;
+
+            if ((matchesEdible || currentFilters.edible == 'all') && (matchesSeason || currentFilters.season == 'all')) {
+                card.hidden = false;
+                hasVisibleCards = true;
+            } else {
+                card.hidden = true;
+            }
+
+            if (hasVisibleCards) {
+                noResultsMsg.hidden = true;
+            } else {
+                noResultsMsg.hidden = false;
+            }
+        });
+    }
+})
